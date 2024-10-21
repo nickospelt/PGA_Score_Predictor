0708bda816898bbb2647e932126e511e8356ab7c
nickospelt <85850695+nickospelt@users.noreply.github.com>
2024-10-21 16:46:32 -0400

    Added proximity to hole to the list of player statistics

--- Data/Merge_Tournament_Results_Player_Data.py
+++ Data/Merge_Tournament_Results_Player_Data.py
@@ -2,13 +2,19 @@ import os
 from pickle import FALSE, TRUE
 import pandas as pd
 
+# Convert proximity to hole averages from a string format to a numerical format
+def format_prox_to_hole(value):
+    feet = int(value.split("'")[0])
+    inch = int(value.split("\"")[0].split(" ")[1])
+    return feet + (inch / 12)
+
 # Aggregate all player statistics for each individual player. Join that data with how each player played at the respective tournament
 def aggregate_player_and_tournament_data():
     main_dir = os.path.dirname(__file__)
 
     # list of all the tournament_player_data to be aggregated into final large data set
     master_tournament_player_data_df = pd.DataFrame(columns=["TOURNAMENT_NAME", "ROUND_DATE", "ROUND_NUMBER", "ELEVATION", "TEMPERATURE", "PRECIPITATION", "WIND_SPEED", "WIND_DIRECTION", "COURSE_NAME", "COURSE_LOCATION", "PAR", "LENGTH", "COURSE_AVERAGE_SCORE", 'PLAYER_ID', 'PLAYER_NAME', 'GIR_PERCENTAGE', 
-    'BIRDIE_TO_BOGEY_RATIO', 'AVG_TOTAL_DRIVING_DISTANCE', 'FIR_PERCENTAGE', 'SCORING_AVERAGE', 'SCRAMBLING_PERCENTAGE', 'AVG_THREE_PUTTS_PER_ROUND', 'AVG_PUTTS_PER_ROUND', 
+    'AVG_PROX_TO_HOLE', 'BIRDIE_TO_BOGEY_RATIO', 'AVG_TOTAL_DRIVING_DISTANCE', 'FIR_PERCENTAGE', 'SCORING_AVERAGE', 'SCRAMBLING_PERCENTAGE', 'AVG_THREE_PUTTS_PER_ROUND', 'AVG_PUTTS_PER_ROUND', 
     "SCORE", "TOTAL_SCORE"])
 
     # repeat this process for each tournament result file
@@ -25,6 +31,10 @@ def aggregate_player_and_tournament_data():
         scrambling_percentage_df = pd.read_csv(f'{player_data_path}/scrambling_percentage.csv')
         three_putts_per_round_df = pd.read_csv(f'{player_data_path}/three_putts_per_round.csv')
         putts_per_round_df = pd.read_csv(f'{player_data_path}/putts_per_round.csv')
+        prox_to_hole_df = pd.read_csv(f'{player_data_path}/prox_to_hole.csv')
+
+        # reformat data
+        prox_to_hole_df['AVG'] = prox_to_hole_df.apply(lambda x: format_prox_to_hole(x['AVG']), axis=1)
 
         # merge the player statistic dataframes together and format the final dataframe
         main_player_data_df = pd.merge(gir_percentage_df, birdie_to_bogey_ratio_df, on='PLAYER_ID', how='inner')
@@ -41,10 +51,14 @@ def aggregate_player_and_tournament_data():
         main_player_data_df = main_player_data_df[['PLAYER_ID', 'PLAYER_NAME', 'GIR_PERCENTAGE', 'BIRDIE_TO_BOGEY_RATIO', 'AVG_TOTAL_DRIVING_DISTANCE', 'FIR_PERCENTAGE', 'SCORING_AVERAGE', 'SCRAMBLING_PERCENTAGE', 'AVG']].rename(columns={'AVG': 'AVG_THREE_PUTTS_PER_ROUND'})
         main_player_data_df = pd.merge(main_player_data_df, putts_per_round_df, on='PLAYER_ID', how='inner')
         main_player_data_df = main_player_data_df[['PLAYER_ID', 'PLAYER_NAME', 'GIR_PERCENTAGE', 'BIRDIE_TO_BOGEY_RATIO', 'AVG_TOTAL_DRIVING_DISTANCE', 'FIR_PERCENTAGE', 'SCORING_AVERAGE', 'SCRAMBLING_PERCENTAGE', 'AVG_THREE_PUTTS_PER_ROUND', 'AVG']].rename(columns={'AVG': 'AVG_PUTTS_PER_ROUND'})
+        main_player_data_df = pd.merge(main_player_data_df, prox_to_hole_df, on='PLAYER_ID', how='inner')
+        main_player_data_df = main_player_data_df[['PLAYER_ID', 'PLAYER_NAME', 'GIR_PERCENTAGE', 'BIRDIE_TO_BOGEY_RATIO', 'AVG_TOTAL_DRIVING_DISTANCE', 'FIR_PERCENTAGE', 'SCORING_AVERAGE', 'SCRAMBLING_PERCENTAGE', 'AVG_THREE_PUTTS_PER_ROUND', 'AVG_PUTTS_PER_ROUND', 'AVG']].rename(columns={'AVG': 'AVG_PROX_TO_HOLE'})
+        
+
 
         # reorder the columns
         main_player_data_df['TOURNAMENT_NAME'] = tournament_result_csv.replace('.csv', '')
-        main_player_data_df = main_player_data_df[['PLAYER_ID', 'PLAYER_NAME', 'TOURNAMENT_NAME', 'GIR_PERCENTAGE', 'BIRDIE_TO_BOGEY_RATIO', 'AVG_TOTAL_DRIVING_DISTANCE', 'FIR_PERCENTAGE', 'SCORING_AVERAGE', 'SCRAMBLING_PERCENTAGE', 'AVG_THREE_PUTTS_PER_ROUND', 'AVG_PUTTS_PER_ROUND']]
+        main_player_data_df = main_player_data_df[['PLAYER_ID', 'PLAYER_NAME', 'TOURNAMENT_NAME', 'GIR_PERCENTAGE', 'AVG_PROX_TO_HOLE', 'BIRDIE_TO_BOGEY_RATIO', 'AVG_TOTAL_DRIVING_DISTANCE', 'FIR_PERCENTAGE', 'SCORING_AVERAGE', 'SCRAMBLING_PERCENTAGE', 'AVG_THREE_PUTTS_PER_ROUND', 'AVG_PUTTS_PER_ROUND']]
 
         # print and save the aggregated player data for the respective tournament
         print(f"{tournament_result_csv.replace('.csv', '')} Player Stats Dataframe:")
@@ -55,7 +69,7 @@ def aggregate_player_and_tournament_data():
 
         tournament_player_data_df = pd.merge(main_player_data_df, tournament_result_df, on=['PLAYER_NAME', 'TOURNAMENT_NAME']).sort_values(by='TOTAL_SCORE', ascending=FALSE).reset_index(drop=True)
         tournament_player_data_df = tournament_player_data_df[["TOURNAMENT_NAME", "ROUND_DATE", "ROUND_NUMBER", "ELEVATION", "TEMPERATURE", "PRECIPITATION", "WIND_SPEED", "WIND_DIRECTION", "COURSE_NAME", "COURSE_LOCATION", "PAR", "LENGTH", "COURSE_AVERAGE_SCORE", 'PLAYER_ID', 'PLAYER_NAME', 'GIR_PERCENTAGE', 
-    'BIRDIE_TO_BOGEY_RATIO', 'AVG_TOTAL_DRIVING_DISTANCE', 'FIR_PERCENTAGE', 'SCORING_AVERAGE', 'SCRAMBLING_PERCENTAGE', 'AVG_THREE_PUTTS_PER_ROUND', 'AVG_PUTTS_PER_ROUND', "SCORE", "TOTAL_SCORE"]]
+    'AVG_PROX_TO_HOLE', 'BIRDIE_TO_BOGEY_RATIO', 'AVG_TOTAL_DRIVING_DISTANCE', 'FIR_PERCENTAGE', 'SCORING_AVERAGE', 'SCRAMBLING_PERCENTAGE', 'AVG_THREE_PUTTS_PER_ROUND', 'AVG_PUTTS_PER_ROUND', "SCORE", "TOTAL_SCORE"]]
 
         print(f"{tournament_result_csv.replace('.csv', '')} Final Dataframe:")
         print(tournament_player_data_df)
